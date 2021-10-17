8154571824d94a477f42f6a67243bcf98ab2702b
luketn <lthompson@infomedia.com.au>
2021-10-18 09:48:36 +1100

    switch to docker-compose

--- docker-mongo.sh
+++ docker-mongo.sh
@@ -1,6 +1,6 @@
 # Run a local instance of Mongo for debugging and local development as a Docker daemon (-d) process
 mkdir -p "$(pwd)/data"
-docker run -d --rm --name MongoDBLocal -p 27017:27017 -v "$(pwd)/data:/etc/mongo" mongo:5.0.3 mongod
+docker-compose up -d mongodb
 
 # After you have the Daemon running, you can use the Mongo CLI for ad-hoc queries like this:
-# docker exec -it MongoDBLocal mongo
+# docker exec -it MongoDBLocal mongosh
