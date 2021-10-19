a8d0981f572e8e0263abc67b4a91f294d39ddd1b
luketn <lthompson@infomedia.com.au>
2021-10-19 21:16:42 +1100

    clean up imports

--- src/main/java/com/mycodefu/example/MainVerticle.java
+++ src/main/java/com/mycodefu/example/MainVerticle.java
@@ -1,10 +1,7 @@
 package com.mycodefu.example;
 
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
-import io.vertx.core.*;
-import io.vertx.core.json.JsonObject;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
